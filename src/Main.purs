5f9fc04b32771824061bec2f9322eaae3dbcc9f8
minikomi <nerdfunk@gmail.com>
2015-01-08 16:13:11 +0900

    add ranking of death points

--- src/Main.purs
+++ src/Main.purs
@@ -5,6 +5,9 @@ import Control.Monad.Eff.Random
 import Control.Monad.ST
 
 import Data.Array
+import Data.Map(fromList, toList, Map(), update)
+import Data.Tuple
+import Data.Maybe
 import Data.Maybe.Unsafe
 import Control.Monad
 import Math (floor)
@@ -18,16 +21,21 @@ import Graphics.Canvas.Free
 import Debug.Trace
 
 worldSize :: Number
-worldSize = 24
+worldSize = 13
 
 blockSize :: Number
-blockSize = 10
+blockSize = 20
 
 canvasSize :: Number
 canvasSize = (worldSize * blockSize)
 
-data Direction = Null | U | R | D | L
+startLevel :: Number
+startLevel = 10
 
+maxStep :: Number
+maxStep = 20
+
+data Direction = Null | U | R | D | L
 
 instance showDirection :: Show Direction where
   show U = "U"
@@ -62,13 +70,17 @@ getX (Point p) = p.x
 getY :: Point -> Number
 getY (Point p) = p.y
 
-
 instance showPoint :: Show Point where
   show p = "{"  ++ (show $ getX p) ++ " / " ++ (show $ getY p) ++ "}"
 
 instance eqPoint :: Eq Point where
   (==) (Point p1) (Point p2) = (p1.x == p2.x) && (p1.y == p2.y)
   (/=) p1 p2 = not $ p1 == p2
+
+instance ordPoint :: Ord Point where
+  compare (Point p1) (Point p2) = case compare p1.x p2.x of
+                                       EQ -> compare p1.y p2.y
+                                       o  -> o
 
 type Food = Point
 
@@ -91,17 +103,14 @@ type Snake = {
   , direction :: Direction
   }
 
-defaultSnake :: Number -> Number -> Snake
-defaultSnake w h = {body: initialBody, direction: Null}
+initialSnake :: Number -> Number -> Snake
+initialSnake w h = {body: initialBody, direction: Null}
   where initialBody  = [Point {x: floor(w/2), y: floor(h/2)}]
 
-type DeathPoint = {
-    point :: Point
-  , value :: Number
-  }
+type DeathPoints = Map Point Number
 
 type World = {
-    dps  :: [DeathPoint]
+    dps  :: DeathPoints
   , food :: Food
   , snake :: Snake
   , state :: GameState
@@ -111,22 +120,23 @@ type World = {
   }
 
 
-randomWorld :: forall e. Number -> Number -> Eff ( random :: Random | e ) World
-randomWorld w h = do
-  let initialSnake = defaultSnake w h
-      initialDps   = do x <- 0 .. (w - 1)
-                        y <- 0 .. (h - 1)
-                        return $ {point: Point {x: x, y: y}, value: 0}
-  initialFood      <- randomFood worldSize worldSize initialSnake
+initialWorld :: forall e. Number -> Number -> Eff ( random :: Random, trace :: Trace | e ) World
+initialWorld w h = do
+  let s   = initialSnake w h
+      dps = fromList $ do x <- 0 .. (w - 1)
+                          y <- 0 .. (h - 1)
+                          return $ Tuple (Point {x: x, y: y}) 1
 
-  return { dps: initialDps
-         , food: initialFood
-         , snake: initialSnake
+  f <- randomFood worldSize worldSize s
+  return { dps: dps
+         , food: f
+         , snake: s
          , state: Alive
-         , level: 2
-         , untilStep: 10
+         , level: startLevel
+         , untilStep: maxStep
          , nextDirection: Null
          }
+
 
 drawSnakePart _ bodypart = do rect $ { x: (getX bodypart * blockSize)
                                      , y: (getY bodypart * blockSize) 
@@ -158,9 +168,8 @@ centerText style font message yOffset = do
   beginPath
   setFillStyle style
   setFont font
-  txtsize <- measureText message     
+  txtsize <- measureText message
   fillText message ((canvasSize - txtsize.width)/2) yOffset
-
 
 drawGameOver = do
   centerText "#222" "40px Serif" "GAME OVER" ((canvasSize/2) + 20)
@@ -174,16 +183,54 @@ clear = do
   fill
   closePath
 
+
+-- colorbrewer single hue 10 color range (green)
+colorRange = ["#f7fcf5"
+             ,"#e5f5e0"
+             ,"#c7e9c0"
+             ,"#a1d99b"
+             ,"#74c476"
+             ,"#41ab5d"
+             ,"#238b45"
+             ,"#006d2c"
+             ,"#00441b"]
+
+drawDeathPoint minv maxv _ p = do
+
+  let colorIndex = floor $ (((snd p)- minv) / maxv) * (length colorRange - 1)
+      color      = fromJust $ colorRange !! colorIndex
+
+  beginPath
+  setFillStyle color
+  rect { x: (getX (fst p) * blockSize), y: (getY (fst p) * blockSize), w: blockSize, h: blockSize }
+  fill
+  closePath
+
+
+drawDeathPoints :: [Tuple Point Number] -> Graphics Unit
+drawDeathPoints dplist = do
+  let sorted = sortBy valueSort dplist
+      min    = snd $ fromJust $ head $ sorted
+      max    = snd $ fromJust $ last $ sorted
+
+  foldM (drawDeathPoint min max) unit sorted
+
+  where valueSort dp1 dp2 = compare (snd dp1 :: Number) (snd dp2 :: Number)
+
+
+
+
 drawWorld :: forall e. Context2D -> World -> Eff (canvas :: Canvas | e) Unit
 drawWorld ctx w = runGraphics ctx do
     clear
+    drawDeathPoints (toList w.dps)
     case w.state of
          Alive -> do drawSnake w
                      drawFood w
          Dead  -> do drawGameOver
 
-update :: forall s e. World -> Eff (st :: ST s, random :: Random, trace :: Debug.Trace.Trace | e) World
-update w = if w.untilStep >= 0 then return $ w {untilStep = w.untilStep - w.level}
+updateWorld :: forall s e. World -> Eff (st :: ST s, random :: Random, trace :: Debug.Trace.Trace | e) World
+updateWorld w = if w.untilStep >= 0 then return $ w {untilStep = w.untilStep - w.level}
            else do 
                   let 
                     s    = w.snake
@@ -208,16 +255,19 @@ update w = if w.untilStep >= 0 then return $ w {untilStep = w.untilStep - w.leve
                                  elemIndex h' body > 0) then Dead else Alive
 
                     level' = if (ateFood && w.level < 10) then w.level + 1 else w.level
+                    dps    = w.dps :: Map Point Number
+                    dps'   = case state' of 
+                                    Alive -> w.dps
+                                    Dead  -> update (\n -> Just (n + 1) :: Maybe Number) h dps
 
                   food' <- if ateFood then randomFood worldSize worldSize snake'
                                       else return $ w.food
 
-                  -- print $ show h'
-
-                  return $ w {snake = snake',
+                  return $ w {dps   = dps',
+                              snake = snake',
                               state = state',
                               food  = food',
-                              untilStep = 20,
+                              untilStep = maxStep,
                               level = level'}
 
 
@@ -225,13 +275,13 @@ update w = if w.untilStep >= 0 then return $ w {untilStep = w.untilStep - w.leve
 tick :: forall s e. Context2D -> STRef s World -> Eff (st :: ST s, random :: Random, canvas :: Canvas, trace :: Debug.Trace.Trace | e ) Unit
 tick ctx st = do
   w  <- readSTRef st
-  when (w.state == Alive) do w' <- update w
+  when (w.state == Alive) do w' <- updateWorld w
                              writeSTRef st w'
                              return unit
 
   drawWorld ctx w
 
-keypress :: forall s e. STRef s World -> DOMEvent -> Eff (st :: ST s, dom :: DOM, random :: Random,trace :: Debug.Trace.Trace | e) Unit
+keypress :: forall s e. STRef s World -> DOMEvent -> Eff (st :: ST s, dom :: DOM, random :: Random, trace :: Debug.Trace.Trace | e) Unit
 keypress st event = do
   w <- readSTRef st
   code <- keyCode event
@@ -244,13 +294,21 @@ keypress st event = do
                           37 | w.snake.direction /= R -> L
                           _  -> w.snake.direction
 
-      -- print $ show newDirection
       writeSTRef st $ w {nextDirection = newDirection}
       return unit
 
     Dead | (code == 32) -> do
-      w' <- randomWorld worldSize worldSize
-      writeSTRef st $ w'
+      let s = initialSnake worldSize worldSize
+
+      f <- randomFood worldSize worldSize s
+      writeSTRef st $ w {
+                          food          = f
+                        , snake         = s
+                        , state         = Alive
+                        , level         = startLevel
+                        , untilStep     = maxStep
+                        , nextDirection = Null
+                        }
       return unit
 
     _ -> return unit
@@ -261,7 +319,7 @@ main = do
   setCanvasHeight canvasSize canvas
 
   ctx <- getContext2D canvas
-  w   <- randomWorld worldSize worldSize
+  w   <- initialWorld worldSize worldSize
   st  <- newSTRef w
 
   addKeyboardEventListener KeydownEvent (keypress st) globalWindow
